ba7e7a4cfecd44fa29b09267d69c7b080c99ddda
xchalarm <tharana4671@gmail.com>
2023-12-01 15:12:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,45 +1,27 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
 using namespace std;
 
-double deg2rad(double deg){
-    return ((deg*M_PI)/180);
-}
-double rad2deg(double rad){
-    return((rad*180)/M_PI);
-}
-double findXComponent(double l1,double l2,double a1,double a2){
-    return ((l1*cos(a1))+(l2*cos(a2)));
-}
-double findYComponent(double l1,double l2,double a1,double a2){
-    return ((l1*sin(a1))+(l2*sin(a2)));
-}
-double pythagoras(double xcomp,double ycomp){
-    return (sqrt(pow(xcomp,2)+(pow(ycomp,2))));
-}
-void showResult(double t,double g){
-   cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
-   cout << "Length of the resultant vector = "<< t << "\n";
-   cout <<"Direction of the resultant vector (deg) = " << g << "\n";
-   cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
-}
 int main(){
-    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
-    cout << "Enter length of the first vector: ";
-    cin >> l1;
-    cout << "Enter direction of the first vector (deg): ";
-    cin >> a1;
-    cout << "Enter length of the second vector: ";
-    cin >> l2;
-    cout << "Enter direction of the second vector (deg): ";
-    cin >> a2;
+    int num;
+    int Even =0;
+    int Odd =0;
     
-    a1 = deg2rad(a1);
-    a2 = deg2rad(a2);
-    xcomp = findXComponent(l1,l2,a1,a2);
-    ycomp = findYComponent(l1,l2,a1,a2);
-    result_vec_length = pythagoras(xcomp,ycomp);
-    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
-
-    showResult(result_vec_length,result_vec_direction);
+    cout << "Enter an integer: ";
+    cin >> num;
+    while (num!=0)
+    {
+     if(num %2==0)
+    {
+    Even++;
+    }
+    else
+    {
+    Odd++;
+    }
+    cout << "Enter an integer: ";
+   cin >> num;    
+    }
+    cout << "#Even numbers = " << Even << endl;
+    cout << "#Odd numbers = " << Odd << endl;
+    return 0;
 }
