3d84d7e76762b2c3faf5acd424d425cef24ea27d
xchalarm <tharana4671@gmail.com>
2023-12-01 14:48:26 +0700

    lab6_2.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,26 +1,45 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
+double deg2rad(double deg){
+    return ((deg*M_PI)/180);
+}
+double rad2deg(double rad){
+    return((rad*180)/M_PI);
+}
+double findXComponent(double l1,double l2,double a1,double a2){
+    return ((l1*cos(a1))+(l2*cos(a2)));
+}
+double findYComponent(double l1,double l2,double a1,double a2){
+    return ((l1*sin(a1))+(l2*sin(a2)));
+}
+double pythagoras(double xcomp,double ycomp){
+    return (sqrt(pow(xcomp,2)+(pow(ycomp,2))));
+}
+void showResult(double t,double g){
+   cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+   cout << "Length of the resultant vector = "<< t << "\n";
+   cout <<"Direction of the resultant vector (deg) = " << g << "\n";
+   cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 int main(){
-    int contodd=0,conteven=0,num[50];
-    int i=0;
-    while (1)
-    {
-        cout << "Enter an integer: ";
-        cin>>num[i];
-        if(num[i]==0){
-            break;
-        }
-        if(num[i]%2==0){
-            conteven++;
-        } 
-        if(num[i]%2==1){
-            contodd++;
-        }
-        i++;
-    }
+    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+    cout << "Enter length of the first vector: ";
+    cin >> l1;
+    cout << "Enter direction of the first vector (deg): ";
+    cin >> a1;
+    cout << "Enter length of the second vector: ";
+    cin >> l2;
+    cout << "Enter direction of the second vector (deg): ";
+    cin >> a2;
     
-    cout << "#Even numbers = "<<conteven<<"\n";
-    cout << "#Odd numbers = "<<contodd;
-    return 0;
+    a1 = deg2rad(a1);
+    a2 = deg2rad(a2);
+    xcomp = findXComponent(l1,l2,a1,a2);
+    ycomp = findYComponent(l1,l2,a1,a2);
+    result_vec_length = pythagoras(xcomp,ycomp);
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
+
+    showResult(result_vec_length,result_vec_direction);
 }
